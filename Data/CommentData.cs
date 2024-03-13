fd1c61cb963c4c2a9ff034b1af9338ab3c62666c
Ryan Shore <mshorecode@gmail.com>
2024-03-12 20:26:42 -0500

    setup CommentData.cs and PostData.cs with seed data

--- Data/CommentData.cs
+++ Data/CommentData.cs
@@ -4,6 +4,10 @@ namespace NetflixRareBackend.Data
 {
     public class CommentData
     {
-        public static List<Comment> Comments = new List<Comment>();
+        public static List<Comment> Comments = new List<Comment>
+        {
+            new Comment { Id = 1, Author_Id = 1, Post_Id = 2, Content = "Really insightful article, thanks for sharing!", Created_On = new DateTime(2024-03-04) },
+            new Comment { Id = 2, Author_Id = 2, Post_Id = 1, Content = "I never knew quantum computing was so fascinating.", Created_On = new DateTime(2024-02-25)}
+        };
     }
 }
