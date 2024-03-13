eeca883b1fa79607033839ad668f82be1ffd5d46
Keana Cobarde <keanacobarde@gmail.com>
2024-03-12 19:59:16 -0500

    Adding nullable attributes within classes

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -6,6 +6,6 @@
         public int Author_Id { get; set; }
         public int Post_Id { get; set; }
         public string Content { get; set; }
-        public DateTime Created_On { get; set; }
+        public DateTime? Created_On { get; set; }
     }
 }
