2a58c8eaaf51db995389e4721815ccf7f88581c3
Keana Cobarde <keanacobarde@gmail.com>
2024-03-13 17:53:56 -0500

    Creating GET request for Posts

--- APIs/PostAPIs.cs
+++ APIs/PostAPIs.cs
@@ -3,6 +3,23 @@
     public class PostAPIs
     {
         public static void Map(WebApplication app)
-        { }
+        {
+            app.MapGet("/api/posts", (RareDbContext db) => { 
+                return db.Posts.ToList();
+            });
+
+            //app.MapGet("/api/posts/{userId}", (RareDbContext db, int userId) =>
+            //{
+            //    try
+            //    {
+            //        var userPosts = db.Posts.Where(p => );
+            //    }
+            //    catch 
+            //    {
+            //        return Results.NotFound();
+            //    }
+            //});
+
+        }
     }
 }
