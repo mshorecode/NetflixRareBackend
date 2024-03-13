80ded8a5f1bbbd56660326c0b4d39eb0a0570c2c
cnfairall <101303134+cnfairall@users.noreply.github.com>
2024-03-13 18:00:53 -0500

    sub apis

--- APIs/SubscriptionAPIs.cs
+++ APIs/SubscriptionAPIs.cs
@@ -1,8 +1,29 @@
-﻿namespace NetflixRareBackend.APIs
+﻿using NetflixRareBackend.Models;
+
+namespace NetflixRareBackend.APIs
 {
     public class SubscriptionAPIs
     {
         public static void Map(WebApplication app)
-        { }
+        {
+            app.MapPost("/api/subscriptions", (RareDbContext db, Subscription subscription) =>
+            {
+                db.Subscriptions.Add(subscription);
+                db.SaveChanges();
+                return Results.Created($"/api/subscriptions/{subscription.Id}", subscription);
+            });
+
+            app.MapDelete("/api/subscriptions/{id}", (RareDbContext db, int id) =>
+            {
+                Subscription subToDelete = db.Subscriptions.SingleOrDefault(s => s.Id == id);
+                if (subToDelete != null)
+                {
+                    db.Subscriptions.Remove(subToDelete);
+                    db.SaveChanges();
+                    return Results.NoContent();
+                }
+                return Results.BadRequest("Subscription not found");
+            });
+        }
     }
 }
