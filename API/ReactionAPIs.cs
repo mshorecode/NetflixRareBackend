850211ec7dbc70b60e0b98c1d934b6d11603ce91
Keana Cobarde <keanacobarde@gmail.com>
2024-03-13 21:15:46 -0500

    Adding API to react to a post

--- API/ReactionAPIs.cs
+++ API/ReactionAPIs.cs
@@ -1,4 +1,7 @@
-﻿namespace NetflixRareBackend.APIs
+﻿using Microsoft.EntityFrameworkCore;
+using NetflixRareBackend.Dto;
+
+namespace NetflixRareBackend.APIs
 {
     public class ReactionAPIs
     {
@@ -8,6 +11,30 @@
             app.MapGet("/api/reactions", (RareDbContext db) => 
             { 
                 return db.Reactions.ToList(); 
+            });
+
+            app.MapPost("/api/post/addReaction", (RareDbContext db, PostReactionDto newReaction) => 
+            {
+                var postToReact = db.Posts.Include(p => p.Reactions).FirstOrDefault(p => p.Id == newReaction.Post_Id);
+
+                if (postToReact == null)
+                { 
+                    return Results.NotFound();
+                }
+
+                var reactionToAdd = db.Reactions.Find(newReaction.Reactions_Id);
+
+
+                if (reactionToAdd == null)
+                {
+                    return Results.NotFound();
+                }
+
+                postToReact.Reactions.Add(reactionToAdd);
+
+                db.SaveChanges();
+
+                return Results.Ok();
             });
         }
     }
