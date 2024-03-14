fb252dd4204c1dd7dcea69aad348062339ac1c25
Keana Cobarde <keanacobarde@gmail.com>
2024-03-13 21:05:20 -0500

    Adding PostReaction DTO

---
+++ Dto/PostReactionDto.cs
@@ -0,0 +1,9 @@
+﻿namespace NetflixRareBackend.Dto
+{
+    public class PostReactionDto
+    {
+        public int User_Id { get; set; }
+        public int Post_Id { get; set; }
+        public int Reactions_Id { get; set; }
+    }
+}
